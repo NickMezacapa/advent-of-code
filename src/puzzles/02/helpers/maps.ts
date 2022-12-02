f7dba260af157f6e6e3d2b503f9762c37566a68e
NickMezacapa <89874146+NickMezacapa@users.noreply.github.com>
2022-12-02 13:43:09 -0800

    build: add helpers for day 2 solution

---
+++ src/puzzles/02/helpers/maps.ts
@@ -0,0 +1,23 @@
+export const inputMap = new Map([
+    ["A", "rock"],
+    ["B", "paper"],
+    ["C", "scissors"],
+    ["X", "rock"],
+    ["Y", "paper"],
+    ["Z", "scissors"],
+]);
+export const scoreMap = new Map([
+    ["rock", 1],
+    ["paper", 2],
+    ["scissors", 3],
+]);
+export const gameMap = new Map([
+    ["tie", 3],
+    ["win", 6],
+    ["loss", 0],
+]);
+export const patternMap = new Map([
+    ["Y", "tie"],
+    ["Z", "win"],
+    ["X", "loss"],
+]);
