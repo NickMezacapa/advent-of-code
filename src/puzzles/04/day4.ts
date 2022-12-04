286df2cce40df326b1a90331c804bdb7330c318d
NickMezacapa <89874146+NickMezacapa@users.noreply.github.com>
2022-12-04 15:38:04 -0800

    add day 4

---
+++ src/puzzles/04/day4.ts
@@ -0,0 +1,8 @@
+import { Entries } from "@/lib/types";
+import * as fs from "fs";
+
+const inputFile: string = "src/puzzles/04/test/mockInput.txt";
+let inputContent = fs.readFileSync(inputFile, "utf8");
+
+
+export default class PuzzleSolution {}
---
+++ src/puzzles/04/test/day4.spec.ts
@@ -0,0 +1,1 @@
+import PuzzleSolution from "../day4";
