c84c4dee74b8daf6435f8c04e64956f6488dbce2
NickMezacapa <89874146+NickMezacapa@users.noreply.github.com>
2022-12-05 16:02:23 -0800

    test: add unit test

--- src/puzzles/04/test/day4.spec.ts
+++ src/puzzles/04/test/day4.spec.ts
@@ -1,1 +1,11 @@
 import PuzzleSolution from "../day4";
+
+describe("Day 4 - Camp Cleanup", () => {
+    describe("first puzzle", () => {
+        describe("find elves who's cleanup assignment overlaps with another elf's", () => {
+            it("should be defined", () => {
+                expect(PuzzleSolution).toBeDefined();
+            });
+        });
+    });
+});
